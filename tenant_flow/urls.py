eed93daccb214b312f2a3a0557cb60e48ef5163a
Nalluri Dheeraj <dheerajnalluri2@gmail.com>
2026-08-03 00:04:00 +0530

    fix: serve media files correctly in production deployment

--- tenant_flow/urls.py
+++ tenant_flow/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -30,7 +31,7 @@ urlpatterns = [
     path("tasks/", include("apps.tasks.urls")),
     path("documents/", include("apps.documents.urls")),
     path("notifications/", include("apps.notifications.urls")),
+    
+    # Serve media files (user uploads) manually in production for the AWS deployment
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
